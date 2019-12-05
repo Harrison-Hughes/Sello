d550dea5287469cf3dbb4a5df12cb8d318de8109
Qing <chingwang.qw@gmail.com>
2019-12-05 09:37:56 +0000

    add flash error message on order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,12 @@ class OrdersController < ApplicationController
 
   def create
     unless session[:user_id]
-      redirect_to new_session
+      redirect_to new_user_path
     end
-
+    
     product = Product.find(params[:order][:product_id])
     if product.stock_count <= 0
-      # TODO: Show an error: sold out!
+      flash[:notice] = "Sorry, this item is sold out!"
       redirect_to product_path(product)
     end
 
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
     buyer = User.find(session[:user_id])
 
     if product.user_id == buyer.id
-      # TODO: Show an error: can't buy your own product!
+      flash[:notice] = "Sorry, you can't buy your own product!"
       redirect_to product_path(product)
     end
 
@@ -34,6 +34,7 @@ class OrdersController < ApplicationController
       status: "NEW",
       total_cost: product.price,
     )
+    
     order_item = OrderProductJoin.create(
       order_id: order.id,
       product_id: product.id,
@@ -42,7 +43,6 @@ class OrdersController < ApplicationController
       product_description: product.description,
       quantity: 1,
     )
-
     redirect_to order_path(order)
   end
 end
