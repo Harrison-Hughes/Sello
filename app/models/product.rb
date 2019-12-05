bb39b898075588518d3af09615cc0c4cebe70295
Qing <chingwang.qw@gmail.com>
2019-12-05 13:56:13 +0000

    add product tag on show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,18 @@
 class Product < ApplicationRecord
-  
-    belongs_to :user
-    has_many :product_tags
-    has_many :tags, through: :product_tags
-    has_many :orders
-    has_many :users, through: :orders
-    has_many :basket_joins
-    has_many :order_product_joins
-    has_many :orders, through: :order_product_joins
+  belongs_to :user
+  has_many :product_tags
+  has_many :tags, through: :product_tags
+  has_many :orders
+  has_many :users, through: :orders
+  has_many :basket_joins
+  has_many :order_product_joins
+  has_many :orders, through: :order_product_joins
 
-    def add_tag_by_id(tag_id)
-      if !ProductTag.find_by(product_id: self.id, tag_id: tag_id)
-        ProductTag.create(product_id: self.id, tag_id: tag_id)
-      else puts "tag already associated with product"
-      end
-    end
+  def add_tag_by_id(tag_id)
+    if !ProductTag.find_by(product_id: self.id, tag_id: tag_id)
+      ProductTag.create(product_id: self.id, tag_id: tag_id)
+    else puts "tag already associated with product"     end
+  end
 
   def remove_tag_by_id(tag_id)
     if ProductTag.find_by(product_id: self.id, tag_id: tag_id)
@@ -31,4 +29,7 @@ class Product < ApplicationRecord
     "%.2f" % self.price
   end
 
+  def tag_name
+    ProductTag.find(self.id).tag.name
+  end
 end
